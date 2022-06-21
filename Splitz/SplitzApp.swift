2ace61bf256a9e8b01e9910d82c881c97e7047f2
julian.s.schmid@icloud.com <julian.s.schmid@icloud.com>
2022-06-21 16:27:12 +0200

    Initial Commit

---
+++ Splitz/SplitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitzApp.swift
+//  Splitz
+//
+//  Created by Julian Schmid on 21.06.22.
+//
+
+import SwiftUI
+
+@main
+struct SplitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
