0b630932f5fcc60f4efb03e27ea7e3f8fe644193
Julian Schmid <68326859+KingNexu@users.noreply.github.com>
2022-07-12 09:55:39 +0200

    Create UserDao

--- Splitz/Dao/UserDao.swift
+++ Splitz/Dao/UserDao.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+import CoreData
+
+class UserDao: Dao<User, CdUser> {
+    
+    //Get By UID
+    
+    func getByUid(userId: UUID) async throws -> User? {
+        let cdUser: CdUser? = try await getByUid(userId: userId)
+        return cdUser.map { $0.decode() }
+    }
+    
+    private func getByUid(userId: UUID) async throws -> CdUser? {
+
+        let backgroundContext = storage.taskContext
+        
+        let cdUser: CdUser? = try await fetchObject(backgroundContext: backgroundContext, predicates: [NSPredicate(format: "id = %@", userId.uuidString)])
+        
+        return cdUser
+
+    }
+}
