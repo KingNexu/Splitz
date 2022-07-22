825b738b8a0fc5c5db273e5171306f720447ca0c
Julian Schmid <68326859+KingNexu@users.noreply.github.com>
2022-07-22 12:55:43 +0200

    #10 Bill View Model Created Model

--- Splitz/Views/Main/MainViewViewModel.swift
+++ Splitz/Views/Main/MainViewViewModel.swift
@@ -5,14 +5,34 @@
 //  Created by Julian Schmid on 12.07.22.
 //
 
-//import Foundation
-//
-//final class MainViewModel {
-//
-//    private let billRepository: BillRepository
-//
-//    required init {
-//    billRepository: BillRepository = Injector
-//    }
-//
-//}
+import Foundation
+
+final class MainViewViewModel: ViewModel {
+
+    private let billRepository: BillRepository
+
+    required init (
+        billRepository: BillRepository = Injector.getBillRepository()
+    ) {
+        self.billRepository = billRepository
+    }
+    
+    func getAllBills() async throws -> [Bill]{
+        do {
+            return try await billRepository.getAll()
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
+    }
+    
+    func deleteBill(id: UUID) async throws {
+        do {
+            try await billRepository.deleteBill(id: id)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+
+}
